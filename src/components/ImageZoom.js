f7189ea745cc325336706bb8e646c34b217c2ec9
gauravkumarchhipa <gauravchhipa295@gmail.com>
2023-03-23 15:14:00 +0530

    First Commit

---
+++ src/components/ImageZoom.js
@@ -0,0 +1,40 @@
+import ImageGallery from "react-image-gallery";
+import PinchZoom from "react-pinch-and-zoom";
+import "../app.css";
+const images = [
+  {
+    original: "https://source.unsplash.com/random/200x200?sig=1",
+    
+  },
+ 
+];
+export default function ImageZoom() {
+  const renderItem = (item) => {
+    return (
+      <PinchZoom>
+        <img
+          src={item.original}
+          alt={item.originalAlt}
+          style={{ maxWidth: "100%", maxHeight: "100%", objectFit: "contain" }}
+          
+        />
+      </PinchZoom>
+    );
+  };
+
+  return (
+    <div className="image">
+      <ImageGallery
+        items={images}
+        renderItem={renderItem}
+        showFullscreenButton={true}
+        useBrowserFullscreen={true}
+        showPlayButton={false}
+        autoPlay={true}
+        
+      />
+    </div>
+  );
+}
+
+
