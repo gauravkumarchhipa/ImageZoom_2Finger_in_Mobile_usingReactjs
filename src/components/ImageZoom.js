5190d2cc4b54ad64960e71c91907bd5003ad70f5
gauravkumarchhipa <gauravchhipa295@gmail.com>
2023-03-24 11:19:27 +0530

    second Commit

--- src/components/ImageZoom.js
+++ src/components/ImageZoom.js
@@ -11,7 +11,7 @@ const images = [
 export default function ImageZoom() {
   const renderItem = (item) => {
     return (
-      <PinchZoom>
+      <PinchZoom className={''} debug={false} minZoomScale={0} maxZoomScale={0} boundSize={undefined} contentSize={undefined}> 
         <img
           src={item.original}
           alt={item.originalAlt}
